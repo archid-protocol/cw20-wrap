59e33efdbb4196a2016477e46e0054135b38d36d
Drew Taylor <drew@chainofinsight.com>
2023-10-23 14:35:08 -0400

    Refactor -> mover execute functions from contract.rs to execute.rs

--- src/contract.rs
+++ src/contract.rs
@@ -1,11 +1,7 @@
 use cosmwasm_std::{
-    entry_point, to_binary, BankMsg, Binary, Coin, CosmosMsg, Deps, DepsMut, Env, MessageInfo,
+    entry_point, to_binary, Binary, Deps, DepsMut, Env, MessageInfo,
     Response, StdResult, Uint128,
 };
-
-use crate::error::ContractError;
-use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
-use crate::state::{State, STATE};
 
 use cw2::set_contract_version;
 use cw20_base::allowances::{
@@ -13,11 +9,16 @@ use cw20_base::allowances::{
     execute_transfer_from, query_allowance,
 };
 use cw20_base::contract::{
-    execute_burn, execute_mint, execute_send, execute_transfer, query_balance, query_minter,
+    execute_burn, execute_send, execute_transfer, query_balance, query_minter,
     query_token_info,
 };
 use cw20_base::enumerable::{query_all_accounts, query_owner_allowances};
-use cw20_base::state::{BALANCES, MinterData, TokenInfo, TOKEN_INFO};
+use cw20_base::state::{MinterData, TokenInfo, TOKEN_INFO};
+
+use crate::error::ContractError;
+use crate::execute::{ try_deposit, try_withdraw };
+use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::state::{State, STATE};
 
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:cw20-wrap";
@@ -111,72 +112,6 @@ pub fn execute(
     }
 }
 
-pub fn try_deposit(deps: DepsMut, env: Env, info: MessageInfo) -> Result<Response, ContractError> {
-    let state = STATE.load(deps.storage)?;
-
-    let deposit = info
-        .funds
-        .iter()
-        .find(|x| x.denom == state.native_coin)
-        .ok_or(ContractError::InvalidDeposit {
-            denom: state.native_coin,
-        })?;
-
-    let sub_info = MessageInfo {
-        sender: env.contract.address.clone(),
-        funds: vec![],
-    };
-    execute_mint(
-        deps,
-        env,
-        sub_info,
-        info.sender.clone().into(),
-        deposit.amount,
-    )?;
-
-    let res = Response::new()
-        .add_attribute("action", "deposit")
-        .add_attribute("amount", deposit.amount)
-        .add_attribute("sender", info.sender);
-    Ok(res)
-}
-
-pub fn try_withdraw(
-    deps: DepsMut,
-    env: Env,
-    info: MessageInfo,
-    amount: Uint128,
-) -> Result<Response, ContractError> {
-    let state = STATE.load(deps.storage)?;
-
-    // Ensure user has balance sufficient to cover withdrawal
-    let balance = BALANCES
-        .may_load(deps.storage, &info.sender)?
-        .unwrap_or_default();
-    if u128::from(amount) > u128::from(balance) {
-        return Err(ContractError::InvalidWithdrawal {
-            withdrawal: amount.into(),
-            balance: balance.into(),
-        });
-    }
-
-    // Burn coins
-    execute_burn(deps, env, info.clone(), amount)?;
-
-    // Return native coin
-    let bank_send = CosmosMsg::Bank(BankMsg::Send {
-        to_address: info.sender.clone().into(),
-        amount: vec![Coin::new(amount.into(), state.native_coin)],
-    });
-
-    let res = Response::new()
-        .add_attribute("action", "withdraw")
-        .add_attribute("amount", amount)
-        .add_attribute("sender", info.sender)
-        .add_message(bank_send);
-    Ok(res)
-}
-
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
@@ -201,11 +136,13 @@ pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use cosmwasm_std::{
+        BankMsg, coin, coins, CosmosMsg, from_binary, SubMsg
+    };
     use cosmwasm_std::testing::{
         mock_dependencies, mock_dependencies_with_balance, mock_env, mock_info,
     };
     use cw20::BalanceResponse;
-    use cosmwasm_std::{coin, coins, from_binary, SubMsg};
 
     #[test]
     fn proper_initialization() {
